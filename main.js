e26c111b9e262d1cee7fa3f2f399129f437410db
Lehotaiabel <cqde.hu@gmail.com>
2023-08-19 01:54:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-var name = $('#name_input').val()
-var price = $('#price_input').val()
-var l0cation = $('#location_input').val()
-var area = $('#area_input').val()
-
 $(document).ready(function () {
     $('#add_button').click(function () {
+        var name = $('#name_input').val();
+        var price = $('#price_input').val();
+        var location = $('#location_input').val();
+        var area = $('#area_input').val();
+
         $.ajax({
             type: "POST",
             url: "add_offer.php",
-            data: {name: name, price: price, location: l0cation, area: area},
+            data: { name: name, price: price, location: location, area: area },
             success: function (response) {
-                alert('Done')
+                alert('Done');
             }
         });
-    })
+    });
 });
 
 //setInterval(function () {
