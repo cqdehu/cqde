2d8ae3f5e05b55e932f8da43c9a1bbdca22f9b5d
Lehotaiabel <cqde.hu@gmail.com>
2023-08-19 01:45:49 +0200

    start

---
+++ add_offer.php
@@ -0,0 +1,29 @@
+<?php
+
+$servername = "localhost";
+$username = "u142909563_LEAdmin";
+$password = "LEadmin23";
+$dbname = "u142909563_LEARN";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Kapcsolódási hiba: " . $conn->connect_error);
+}
+
+$name = $_POST["name"];
+$price = $_POST["price"];
+$location = $_POST["location"];
+$area = $_POST["area"];
+
+$sql = "INSERT INTO school (name, price, location, area) VALUES ('$name', '$price', '$location', '$area')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Adatok sikeresen hozzáadva az adatbázishoz.";
+} else {
+    echo "Hiba: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
---
+++ main.js
@@ -0,0 +1,22 @@
+var name = $('#name_input').val()
+var price = $('#price_input').val()
+var l0cation = $('#location_input').val()
+var area = $('#area_input').val()
+
+$(document).ready(function () {
+    $('#add_button').click(function () {
+        $.ajax({
+            type: "POST",
+            url: "add_offer.php",
+            data: {name: name, price: price, location: l0cation, area: area},
+            success: function (response) {
+
+            }
+        });
+    })
+});
+
+//setInterval(function () {
+//    var name = $('#name_input').val()
+//    console.log(name)
+//}, 1000);
