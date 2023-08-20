e96272ce8698d396bd0c779e9fdade8472f24e3e
Lehotaiabel <cqde.hu@gmail.com>
2023-08-20 20:59:44 +0200

    Update get_offer_name.php

--- get_offer_name.php
+++ get_offer_name.php
@@ -1,32 +1,27 @@
-<select name="" class="form-select" id="list">
-    <option value="">Válassz...</option>
+<?php
+$servername = "localhost";
+$username = "u142909563_LEAdmin";
+$password = "LEadmin23";
+$dbname = "u142909563_LEARN";
 
-    <?php
-    $servername = "localhost";
-    $username = "u142909563_LEAdmin";
-    $password = "LEadmin23";
-    $dbname = "u142909563_LEARN";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Kapcsolódási hiba: " . $conn->connect_error);
+}
 
-    if ($conn->connect_error) {
-        die("Kapcsolódási hiba: " . $conn->connect_error);
+$sql = "SELECT * FROM `school`";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+?>
+        <option value="<?php echo $row['name'] ?>" id="<?php echo $row['name'] ?>"><?php echo mb_strtoupper($row["name"], 'UTF-8') ?></option>
+<?php
     }
+} else {
+    echo "Nincsenek adatok.";
+}
 
-    $sql = "SELECT * FROM `school`";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-    ?>
-            <option value="<?php echo $row['name'] ?>" id="<?php echo $row['name'] ?>"><?php echo mb_strtoupper($row["name"], 'UTF-8') ?></option>
-    <?php
-        }
-    } else {
-        echo "Nincsenek adatok.";
-    }
-
-    $conn->close();
-    ?>
-
-</select>
+$conn->close();
+?>
