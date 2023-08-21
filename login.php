a11ff8d4b2c08477ca91801927d6cc0e8c93cd80
Lehotaiabel <cqde.hu@gmail.com>
2023-08-21 05:16:01 +0200

    Update login.php

--- login.php
+++ login.php
@@ -9,22 +9,26 @@ $dbname = "u142909563_LEARN";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Ellenőrizd, hogy a kapcsolat létrejött-e
-if ($conn->connect_error) {
-    die("Kapcsolódási hiba: " . $conn->connect_error);
-}
-
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
     // Ellenőrizd a bejelentkezési adatokat és végrehajtsd a bejelentkezést
 
-    // Ha sikeres bejelentkezés, akkor elindítjuk a munkamenetet és átirányítjuk a dashboard-ra
-    if ($login_sikeres) {
-        $_SESSION["username"] = $username;
-        header("Location: dashboard.php");
+    $sql = "SELECT password FROM users WHERE username = '$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $hashedPassword = $row["password"];
+
+        // Jelszó ellenőrzése
+        if (password_verify($password, $hashedPassword)) {
+            $_SESSION["username"] = $username;
+            header("Location: dashboard.php");
+        } else {
+            $hiba_uzenet = "Hibás felhasználónév vagy jelszó.";
+        }
     } else {
         $hiba_uzenet = "Hibás felhasználónév vagy jelszó.";
     }
