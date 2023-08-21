eedef6e8db68c5587d75773c988eb6d5b09133ad
Lehotaiabel <cqde.hu@gmail.com>
2023-08-21 02:44:31 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <div class="row">
+    <div class="row row-cols-1">
         <div class="col p-0 m-0">
             <label class="p-0 m-0 text-light">Username</label>
             <input type="text" class="form-control fw-bold border-0 rounded-3 mt-2" id="name_input">
