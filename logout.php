eadc6abbb7e028b858b9e89f569ba70ed4b1709f
Lehotaiabel <cqde.hu@gmail.com>
2023-08-21 05:03:32 +0200

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+// Munkamenet törlése és egyéb szükséges adatok törlése
+session_unset();
+session_destroy();
+
+// Átirányítás a bejelentkezési oldalra
+header("Location: index");
+?>
