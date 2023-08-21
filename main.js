4be63dd687b901dc3bef2453b7c68c848279b46e
Lehotaiabel <cqde.hu@gmail.com>
2023-08-21 03:54:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -143,7 +143,7 @@ $(document).ready(function () {
         var id = $(this).attr("id");
         console.log(id)
         $.ajax({
-            type: "POST",
+            type: "GET",
             url: "get_offer.php",
             data: { id: id },
             success: function (response) {
