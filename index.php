f198d792ca5170f2ac955ec0296b42ed480e9664
Lehotaiabel <cqde.hu@gmail.com>
2023-08-22 01:12:14 +0200

    Update index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+
+    <?php include './includes/header.php' ?>
+
+    <main>
+        <h1>About Us</h1>
+        <p>We are a team of passionate individuals dedicated to creating amazing web experiences.</p>
+    </main>
+</body>
+
+</html>
