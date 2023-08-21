c0f15ac5088ad6fdcaffdaffa1d488e700194500
Lehotaiabel <cqde.hu@gmail.com>
2023-08-21 05:25:55 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -7,6 +7,30 @@ if (!isset($_SESSION["username"])) {
     exit(); // Fontos: Azonnal leállítjuk a további kód végrehajtását
 }
 
+$servername = "localhost";
+$username = "u142909563_LEAdmin";
+$password = "LEadmin23";
+$dbname = "u142909563_LEARN";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Ellenőrizd, hogy a kapcsolat létrejött-e
+if ($conn->connect_error) {
+    die("Kapcsolódási hiba: " . $conn->connect_error);
+}
+
+// Felhasználónevek lekérdezése az adatbázisból
+$sql = "SELECT username FROM users";
+$result = $conn->query($sql);
+
+$felhasznalo_nevek = array();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $felhasznalo_nevek[] = $row["username"];
+    }
+}
+
 // Ha a felhasználó be van jelentkezve, folytathatjuk a dashboard tartalmával
 ?>
 
@@ -19,5 +43,14 @@ if (!isset($_SESSION["username"])) {
     <h2>Üdvözöllek a Dashboardon, <?php echo $_SESSION["username"]; ?>!</h2>
     <p>Ez az oldal csak bejelentkezett felhasználóknak érhető el.</p>
     <a href="logout.php">Kijelentkezés</a>
+
+    <h3>Felhasználó nevek az adatbázisból:</h3>
+        <ul>
+            <?php
+            foreach ($felhasznalo_nevek as $nev) {
+                echo "<li>$nev</li>";
+            }
+            ?>
+        </ul>
 </body>
 </html>
