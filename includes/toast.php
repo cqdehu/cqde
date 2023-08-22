ab39676c3c0ce63aff2eea40f6d638cba4f8e298
Lehotaiabel <cqde.hu@gmail.com>
2023-08-22 23:23:24 +0200

    update

---
+++ includes/toast.php
@@ -0,0 +1,16 @@
+<?php
+
+$text = $_POST['text'];
+$id = $_POST['id'];
+$color = $_POST['color'];
+
+?>
+
+<div class="toast rounded-bottom-4 rounded-top-5 border-0 bg-<?php echo $color ?>" id="<?php echo $id ?>" data-bs-delay="6000">
+    <div class="toast-header rounded-top-4 border-0">
+        <strong class="me-auto name">SYSTEM</strong>
+    </div>
+    <div class="toast-body price">
+        <?php echo $text?>
+    </div>
+</div>
--- js/script.js
+++ js/script.js
@@ -1,3 +1,69 @@
+//TOAST//
+
+//TOAST COUNT
+var toast_count = 0;
+
+//TOAST SYSTEM
+function toast(text, color) {
+    toast_count++;
+
+    $.ajax({
+        type: "POST",
+        url: "toast.php",
+        data: { text: text, id: "toast_" + toast_count, color: color },
+        success: function (response) {
+            $('.toast-container').append(response);
+
+            var toastId = "toast_" + toast_count;
+
+            $('#' + toastId).toast('show');
+        }
+    });
+}
+//
+
+////
+
+
+
+//LOGIN//
+
+//NAVIGATE TO LOGIN PAGE
+$('#login_page_button').click(function() {
+    window.location.href = '/pages/login.php'
+})
+//
+
+//LOGIN SYSTEM
+$('#login_button').click(function() {
+    var username = $('#username_input').val()
+    var password = $('#password_input').val()
+
+    if (username !== '' && password !== '') {
+        $.ajax({
+            type: "POST",
+            url: "/login.php",
+            data: {
+                username: username, password: password,
+            },
+            success: function (response) {
+                if (response === 'success') {
+                    window.location.href = '/index.php'
+                } else {
+                    toast(response,'danger')
+                }
+            } 
+        });
+    } else {
+
+    }
+})
+//
+
+////
+
+
+
 //LOGOUT
 $('#profile').click(function () { 
     $.ajax({
@@ -10,39 +76,4 @@ $('#profile').click(function () {
         }
     });
 });
-
-
-//LOGIN
-$('#login_page_button').click(function() {
-    console.log("dawd")
-    window.location.href = '/pages/login.php'
-})
-
-$('#login_button').click(function() {
-    var username = $('#username_input').val()
-    var password = $('#password_input').val()
-
-    $.ajax({
-        type: "POST",
-        url: "/login.php",
-        data: {
-            username: username, password: password,
-        },
-        success: function (response) {
-            if (response === 'success') {
-                window.location.href = '/index.php'
-            } else {
-                console.log(response)
-            }
-        } 
-    });
-})
-
-
-//SIGNUP
-$('#signup_button').click(function() {
-    $.ajax({
-        type: "GET",
-        url: "signup.php",
-    });
-})
+//
