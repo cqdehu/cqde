369ffe1bd98ab7d8911664a2da52f87a7c32b352
Lehotaiabel <cqde.hu@gmail.com>
2023-08-22 19:13:00 +0200

    Update

--- includes/header.php
+++ includes/header.php
@@ -1,43 +1,25 @@
 <?php
-session_start()
+session_start();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
-
-</head>
-
-<body>
-    <div class="container">
-        <div class="row">
+<div class="container">
+    <div class="row">
+        <div class="col">
+            Menu
+        </div>
+        <?php if (isset($_SESSION['username'])) : ?>
             <div class="col">
-                Menu
-            </div>
-            <?php if (isset($_SESSION['username'])) : ?>
-                <div class="col">
-                    <div class="row">
-                        <div class="col">
-                            <p>
-                                <?php echo ucfirst($_SESSION['username']); ?>
-                            </p>
-                        </div>
-                        <div class="col">
-                            <img src="" alt="">
-                        </div>
+                <div class="row">
+                    <div class="col">
+                        <p>
+                            <?php echo ucfirst($_SESSION['username']); ?>
+                        </p>
+                    </div>
+                    <div class="col">
+                        <img src="" alt="">
                     </div>
                 </div>
-            <?php endif; ?>
-        </div>
+            </div>
+        <?php endif; ?>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"></script>
-
-
-</body>
-
-</html>
+</div>
