c0a34871aff8216e1a7d03af6a50b3ebe5f441ef
Lehotaiabel <cqde.hu@gmail.com>
2023-08-22 23:45:06 +0200

    Update toast.php

--- includes/toast.php
+++ includes/toast.php
@@ -1,8 +1,8 @@
 <?php
 
-$text = $_POST['text'];
-$id = $_POST['id'];
-$color = $_POST['color'];
+$text = isset($_POST['text']) ? htmlspecialchars($_POST['text']) : '';
+$id = isset($_POST['id']) ? htmlspecialchars($_POST['id']) : '';
+$color = isset($_POST['color']) ? htmlspecialchars($_POST['color']) : '';
 
 ?>
 
