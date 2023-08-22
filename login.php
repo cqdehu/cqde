c89ef952993341e58ca80dbe29bf07ac7bb42a43
Lehotaiabel <cqde.hu@gmail.com>
2023-08-22 22:17:01 +0200

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,16 @@
 
 session_start();
 
-include './config/db_config.php';
+$db_host = $_SERVER['DB_HOST'];
+$db_user = $_SERVER['DB_USER'];
+$db_pass = $_SERVER['DB_PASS'];
+$db_name = $_SERVER['DB_NAME'];
+
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+if ($conn->connect_error) {
+    die("Hiba a kapcsolódás során: " . $conn->connect_error);
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
