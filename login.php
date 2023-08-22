861f98e67a173e55ed2bc9ae4f413dac43db92f7
Lehotaiabel <cqde.hu@gmail.com>
2023-08-22 22:57:45 +0200

    Update login.php

--- login.php
+++ login.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION["username"] = $username;
             echo "success";
         } else {
-            echo "faild";
+            echo "fail";
         }
     }
 }
