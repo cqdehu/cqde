46a5fd6d45143bdf1c4dd4e82377f4de9bef3198
Lehotaiabel <cqde.hu@gmail.com>
2023-08-22 19:07:42 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,6 @@ session_start()
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
 
 </head>
@@ -16,16 +15,23 @@ session_start()
 <body>
     <div class="container">
         <div class="row">
+            <div class="col">
+                Menu
+            </div>
             <?php if (isset($_SESSION['username'])) : ?>
                 <div class="col">
-                    <p>
-                        <?php echo $_SESSION['username']; ?>
-                    </p>
+                    <div class="row">
+                        <div class="col">
+                            <p>
+                                <?php echo ucfirst($_SESSION['username']); ?>
+                            </p>
+                        </div>
+                        <div class="col">
+                            <img src="" alt="">
+                        </div>
+                    </div>
                 </div>
             <?php endif; ?>
-            <div class="col">
-                <p>aawdawd</p>
-            </div>
         </div>
     </div>
 
