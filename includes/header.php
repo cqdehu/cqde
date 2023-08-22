4c8682badfa58d43d998910c1e7e377aade72ead
Lehotaiabel <cqde.hu@gmail.com>
2023-08-22 18:40:03 +0200

    Update

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,19 @@
-<div>Header</div>
+<?php
+if (!isset($_SESSION['username'])) {
+} else {
+    exit();
+}
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <p>
+                <?php echo $_SESSION['username']; ?>
+            </p>
+        </div>
+        <div class="col">
+            <img src="" alt="">
+        </div>
+    </div>
+</div>
