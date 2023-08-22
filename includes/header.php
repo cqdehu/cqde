0b149def2f803a11f47b1dd874db89042bb0c50a
Lehotaiabel <cqde.hu@gmail.com>
2023-08-22 19:02:36 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -2,17 +2,36 @@
 session_start()
 ?>
 
-<div class="container">
-    <div class="row">
-        <?php if (isset($_SESSION['username'])): ?>
-        <div class="col">
-            <p>
-                <?php echo $_SESSION['username']; ?>
-            </p>
-        </div>
-        <?php endif; ?>
-        <div class="col">
-            <p>aawdawd</p>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <?php if (isset($_SESSION['username'])) : ?>
+                <div class="col">
+                    <p>
+                        <?php echo $_SESSION['username']; ?>
+                    </p>
+                </div>
+            <?php endif; ?>
+            <div class="col">
+                <p>aawdawd</p>
+            </div>
         </div>
     </div>
-</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"></script>
+
+
+</body>
+
+</html>
